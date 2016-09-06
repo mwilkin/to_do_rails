23ef619c930de03b6505674119e341abe14de246
Matt Wilkin and Noah Yasskin <matt+noah@>
2016-09-06 08:53:12 -0700

    delete, update, edit functionality.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,14 +3,17 @@ class ListsController < ApplicationController
     @lists = List.all
     render :index
   end
+
   def show
     @list = List.find(params[:id])
     render :show
   end
+
   def new
     @list = List.new
     render :new
   end
+
   def create
     @list = List.new(list_params)
     if @list.save
@@ -20,8 +23,29 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list= List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
   def list_params
     params.require(:list).permit(:name, :author, :description)
   end
+
 end
